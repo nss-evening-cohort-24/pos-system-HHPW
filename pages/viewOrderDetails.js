8b6e3a355f8d49a9c17eb2585c157a97e5255b00
cnfairall <101303134+cnfairall@users.noreply.github.com>
2023-09-16 13:09:42 -0500

    viewOrderDetails

--- pages/viewOrderDetails.js
+++ pages/viewOrderDetails.js
@@ -6,7 +6,25 @@ const viewOrderDetails = (obj) => {
   let domstring = '';
   domstring += `
   <h1 id="runningTotal">Running Total$$</h1>
-  <button type="button" class="btn btn-primary">Add Item</button>
-  <button type="button" class="btn btn-secondary">Go to Payment</button>
-  `
-}
+  `;
+
+  obj.orderItems.forEach((item) => {
+    domString += `
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">${item.name}</h5>
+          <p class="card-text bold">${item.price}</p>
+          <i id="edit-item-btn--${item.firebaseKey}" class="fas fa-edit btn btn-info"></i>
+          <i id="delete-item-btn--${item.firebaseKey}" class="btn btn-danger fas fa-trash-alt"></i>
+      </div>
+    </div>`;
+  });
+
+  domstring += `
+    <button type="button" class="btn btn-primary">Add Item</button>
+    <button type="button" class="btn btn-secondary">Go to Payment</button>
+    `;
+  renderToDom('#view', domstring);
+};
+
+export default viewOrderDetails;
