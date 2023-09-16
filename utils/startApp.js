2f3e1a14e41e28bb9955123dd5919ec938c437b5
MaggieChafee <maggie.chafee@gmail.com>
2023-09-16 12:59:37 -0500

    created very basic homepage

--- utils/startApp.js
+++ utils/startApp.js
@@ -1,9 +1,11 @@
 import logoutButton from '../components/logoutButton';
+import welcome from '../pages/welcome';
 import domBuilder from './domBuilder';
 
 const startApp = () => {
   domBuilder();
   logoutButton();
+  welcome();
 };
 
 export default startApp;
