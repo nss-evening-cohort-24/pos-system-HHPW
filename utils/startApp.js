4638ce210348be6aae0eb5ea7114df2fbb286530
Brandon Schnurbusch <b33blbroxx@gmail.com>
2023-09-16 13:17:31 -0500

    adding navbar

--- utils/startApp.js
+++ utils/startApp.js
@@ -1,8 +1,10 @@
 import logoutButton from '../components/logoutButton';
+import navBar from '../components/navBar';
 import domBuilder from './domBuilder';
 
 const startApp = () => {
   domBuilder();
+  navBar();
   logoutButton();
 };
 
