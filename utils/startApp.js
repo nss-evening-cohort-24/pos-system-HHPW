94f4b3531deac99d1d6d7b63d67537dd50ecf4a9
cnfairall <101303134+cnfairall@users.noreply.github.com>
2023-09-18 19:13:42 -0500

    merge with main

--- utils/startApp.js
+++ utils/startApp.js
@@ -1,14 +1,11 @@
 import logoutButton from '../components/logoutButton';
 import welcome from '../pages/welcome';
 import domBuilder from './domBuilder';
-import getOrderDetails from '../api/mergedData';
-import viewOrderDetails from '../pages/viewOrderDetails';
 
 const startApp = () => {
   domBuilder();
   logoutButton();
   welcome();
-  getOrderDetails('010207055-5').then((obj) => viewOrderDetails(obj));
 };
 
 export default startApp;
