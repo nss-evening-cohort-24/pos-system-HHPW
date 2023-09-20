a707207cc8d7debc69e6830fd7620b186b5e60e7
cnfairall <101303134+cnfairall@users.noreply.github.com>
2023-09-19 19:20:54 -0500

    add item form event

--- events/formEvents.js
+++ events/formEvents.js
@@ -1,12 +1,14 @@
 import { createOrder, getOrders, updateOrder } from '../api/orderData';
 import { viewAllOrders } from '../pages/orders';
+import { createOrderItem, updateOrderItem } from '../api/orderItemsData';
+import getOrderDetails from '../api/mergedData';
+import viewOrderDetails from '../pages/viewOrderDetails';
 
 const formEvents = (user) => {
   document.querySelector('#form-container').addEventListener('submit', (e) => {
     e.preventDefault();
 
     if (e.target.id.includes('submit-order')) {
-      console.warn('click check');
       const payload = {
         customerName: document.querySelector('#customerName').value,
         email: document.querySelector('#customerEmail').value,
@@ -20,6 +22,24 @@ const formEvents = (user) => {
         const patchPayload = { firebaseKey: name };
         updateOrder(patchPayload).then(() => {
           getOrders(user.uid).then(viewAllOrders);
+        });
+      });
+    }
+
+    if (e.target.id.includes('submit-add-item')) {
+      const [, orderId] = e.target.id.split('--');
+
+      const payload = {
+        orderId,
+        itemId: document.querySelector('#itemId').value,
+        uid: user.uid
+      };
+
+      createOrderItem(payload).then(({ name }) => {
+        const patchPayload = { firebaseKey: name };
+
+        updateOrderItem(patchPayload).then(() => {
+          getOrderDetails(orderId).then((res) => viewOrderDetails(res));
         });
       });
     }
