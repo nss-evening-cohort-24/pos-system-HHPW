f7ecc6399080e216327c91ee83a9f059c76fe69b
Brandon Schnurbusch <b33blbroxx@gmail.com>
2023-09-23 09:30:56 -0500

    Fixed big logo sizing on welcome and added mini logo to navbar

--- pages/welcome.js
+++ pages/welcome.js
@@ -5,7 +5,7 @@ const welcome = (user) => {
   let domstring = '';
   domstring = `
   <h1 id="greeting">Welcome, ${user.displayName}!</h1>
-  <br><img src="${bigLogo}"><br>
+  <br><img id="logo" src="${bigLogo}"><br>
   <button type="button" class="btn btn-success" id="view-orders-btn">View Orders</button>
   <button type="button" class="btn btn-info" id="create-order-btn">Create Order</button>
   <button type="button" class="btn btn-warning" id="view-revenue-btn">View Revenue</button>
